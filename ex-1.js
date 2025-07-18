a89b618abfde181ed669d4b6ff5c5950a2478ad9
Felipe Rohde <feliperohdee@gmail.com>
2025-07-18 10:35:06 -0300

    Refine content in data-types.md for clarity and accuracy

---
+++ ex-1.js
@@ -0,0 +1,17 @@
+// 1. Declare uma variável chamada `challenge` e atribua a ela um valor inicial **'30 Dias de JavaScript'**.
+
+let challenge = '30 Dias de JavaScript';
+
+// 2. Imprima a string no console do navegador usando __console.log()__
+
+console.log(challenge);
+
+// 3. Imprima o __length__ da string no console do navegador usando _console.log()_
+
+console.log(challenge.length);
+
+// 4. Altere todos os caracteres da string para letras maiúsculas usando o método __toUpperCase()__
+
+console.log(challenge.toUpperCase());
+
+// 5. Altere todos os caracteres da string para letras minúsculas usando o método __toLowerCase()__
