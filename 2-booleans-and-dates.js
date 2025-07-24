0f30c3cab91819320dd189291de6e10d247d9524
Ana Lopes <carol.llopes@hotmail.com>
2025-07-24 11:46:43 -0300

    Add initial implementation of variable declarations and type checks in booleans-and-dates.js

---
+++ 2-booleans-and-dates.js
@@ -0,0 +1,22 @@
+// 1. Declare as variáveis firstName, lastName, country, city, age, isMarried, year e atribua valores a elas. Use o operador typeof para verificar os diferentes tipos de dados.
+let firstName = "Ana";
+let lastName = "Carolina";
+let country = "Brasil";
+let city = "Jaragua do Sul";
+let age = 27;
+let isMarried = true;
+let year = 2025
+
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
+
+// 2. Verifique se o typeof de '10' é igual a 10.
+// 3. Verifique se parseInt('9.8') é igual a 10.
+// 4. O valor booleano é verdadeiro ou falso.
+// 4.1 Escreva três instruções em JavaScript que resultem em um valor truthy.
+// 4.2 Escreva três instruções em JavaScript que resultem em um valor falsy.
